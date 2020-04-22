06e6d8ba3670c6feaf00bb5ded1604dd676adfae
rajdeep <rajdeeprrao94@gmail.com>
2020-04-21 20:36:00 -0400

    Get the item list working and push to firebase

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
-import addIcon from "../../assets/add.svg"
 import clearIcon from "../../assets/clear.svg"
 import "./Item.css"
 
@@ -8,22 +7,7 @@ class Item extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      term: '',
-      items: []
     };
-  }
-
-  onChange = (event) => {
-    this.setState({term: event.target.value});
-    console.log({term: event.target.value})
-  }
-
-  onSubmit = (event) => {
-    event.preventDefault()
-    this.setState({
-      term: '',
-      items: [...this.state.items, this.state.term]
-    });
   }
 
   render() {
@@ -31,15 +15,12 @@ class Item extends Component {
       <div>
         <form className="items" onSubmit={(e) => {
           e.preventDefault()
-          this.formController()
+          this.props.remove(this.props.itemId)
         }}>
-          <TextField/>
-          <TextField className="expires"/>
-          <button className="AddButton">
-            <img src={addIcon}/>
-          </button>
-          <button className="AddButton">
-            <img src={clearIcon}/>
+          <TextField label={this.props.label} value={this.props.itemValue} name="item" />
+          <TextField label={this.props.label} value={this.props.expiresValue} name="expires" className="expires"/>
+          <button type="submit" className="ModifyButton">
+            <img className="image" src={clearIcon}/>
           </button>
         </form>
       </div>
--- src/routes/Dashboard/Dashboard.js
+++ src/routes/Dashboard/Dashboard.js
@@ -10,21 +10,45 @@ import FirebaseApp from '../../libraries/firebase'
 import * as firebase from 'firebase'; // import only storage instead of everythig
 import Header from '../../components/Header/Header'
 import Item from '../../components/Item/Item'
+import ItemForm from '../../components/ItemForm/ItemForm'
 import "./Dashboard.css"
+import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
 
 class Dashboard extends Component{
   constructor(props) {
     super(props);
-    this.state = {
+    this.state ={
+      items:[],
+      count:0,
     };
   }
 
-  itemAdd = () => {
+  remove = (id) => {
+    console.log(id)
+    console.log(this.state)
+    const remainder = this.state.items.filter((item) => {
 
+      if(item.itemId !== id) return item;
+    });
+    // Update state with filter
+    this.setState({items: remainder});
   }
 
-  itemClear = () => {
+  addItem = (itemState) => {
+    this.setState({
+      count:this.state.count+1
+    })
+    let items = this.state.items
+    this.setState({
+      items:[...items,itemState]
+    })
+    console.log([...items,itemState])
+  }
+
+
+
+  submitController = () => {
 
   }
 
@@ -33,29 +57,52 @@ class Dashboard extends Component{
     let db = firebase.firestore(FirebaseApp)
     let currentUser = FirebaseApp.auth().currentUser
     var docData = {
-    stringExample: "Hello world!",
-
+      items: this.state.items,
     }
-    // db.collection("data").doc("one").set(docData).then(function() {
-    //     console.log("Document successfully written!");
-    // });
+    console.log(currentUser)
+    db.collection(currentUser.email).doc("items").set(docData).then(function() {
+        console.log("Document successfully written!");
+    });
   }
 
   render() {
+
+    const ItemList = ({remove}) => {
+      const items = this.state.items.map(function(item){
+        return (
+          <Item
+            key={item['itemId']}
+            itemId={item['itemId']}
+            label="Read Only"
+            type="Clear"
+            remove={remove}
+            itemValue={item['item']}
+            expiresValue={item['expires']}
+            className="listItem"
+            />
+          );
+      });
+      return (<ul>{items}</ul>)
+    }
+
     return (
       <div>
         <Header/>
-        <Item addFunc={() => this.itemAdd()} clearFunc={() => this.itemClear()}/>
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          className="submitButton submit"
-          onClick={this.submitController}
-        >
-          Submit
-        </Button>
+        <div className="margins">
+          <ItemForm itemId={this.state.count} func={(state) => this.addItem(state)}/>
+          <Divider className="dividerClass"/>
+          <ItemList remove={this.remove}/>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className="submitButton submit"
+            onClick={this.submitController}
+          >
+            Submit
+          </Button>
+        </div>
       </div>
     )
   }
